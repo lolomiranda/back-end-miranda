a80f1b5e347f7db9462fa6d6e355bab9d2f3fa74
lolomiranda <lohannypmiranda@gmail.com>
2025-03-25 11:05:08 -0300

    aula devito

---
+++ aula 3/user.js
@@ -0,0 +1,14 @@
+class User {  
+    constructor(id, nome, email, senha, endereco, telefone, cpf){
+        this.id = id;
+        this.nome = nome; 
+        this.email = email;
+        this.senha = senha;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cpf = cpf;
+
+    }  
+}  
+
+module.exports = User;
