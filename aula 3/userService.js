d333c35030d67b101075c81891139b9a8440893e
lolomiranda <lohannypmiranda@gmail.com>
2025-04-22 08:39:25 -0300

    8:40

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -5,42 +5,7 @@ const bcrypt = require('bcryptjs'); //modulo para criptografar senha
 const mysql = require("./mysql")//Importando funções de conexão com o MySQL
 
 class userService {
-    constructor() {
-        this.filePath = path.join(__dirname, 'user.json'); //caminho do arquivo
-        this.users = this.loadUsers(); //Array para armazenar user
-        this.nextId = this.getNextId(); //contador para gerar id
-    }
-
-    loadUsers() { //carregar os usuarios do JSON (Banco)
-        try { //tenta executar o bloco de codigo
-            if (fs.existsSync(this.filePath)) { //verifica se o arquivo existe
-                const data = fs.readFileSync(this.filePath); //le o arquivo
-                return JSON.parse(data); //transforma o json em objeto
-            }
-        } catch (erro) { //caso ocorra um erro
-            console.log('Erro ao carregar arquivo', erro);
-        }
-        return []; //retorna um array vazio
-    }
-
-    //difinir o proximo id a ser utilizado
-    getNextId() {//função para buscar o proximo id
-        try {
-            if (this.users.length === 0) return 1;
-            return Math.max(...this.users.map(user => user.id)) + 1;//retorna o maior id +1
-        } catch (erro) {
-            console.log('Erro ao buscar proximo id', erro);
-        }
-    }
-
-    saveUsers() { //função para salvar os usuarios
-        try {
-            fs.writeFileSync(this.filePath, JSON.stringify(this.users)); //salva o arquivo
-        } catch (erro) {
-            console.log('Erro ao salvar arquivo', erro);
-        }
-    }
-
+ 
     async addUser(nome, email, senha, endereco, telefone, cpf) { //função para adicionar usuario
         try {
             const senhaCripto = await bcrypt.hash(senha, 10);
